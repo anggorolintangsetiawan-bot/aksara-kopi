73ded91a028fa75778492575e9949fffcf6eacde
dewa_anggoro <anggorolintangsetiawan@gmail.com>
2026-05-05 12:17:53 +0700

    fix clean install

--- server/db.js
+++ server/db.js
@@ -1,31 +1,28 @@
 const Database = require('better-sqlite3');
-const db = new Database('./aksara.db');
+const db = new Database('aksara.db');
 
-db.serialize(() => {
+// buat tabel
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS reservasi (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nama TEXT,
+    wa TEXT,
+    tanggal TEXT,
+    jam TEXT,
+    orang TEXT,
+    area TEXT,
+    catatan TEXT
+  )
+`).run();
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS reservasi (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nama TEXT,
-      wa TEXT,
-      tanggal TEXT,
-      jam TEXT,
-      orang TEXT,
-      area TEXT,
-      catatan TEXT
-    )
-  `);
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nama TEXT,
+    items TEXT,
+    total INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`).run();
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS orders (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nama TEXT,
-      items TEXT,
-      total INTEGER,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
-
-});
-
-module.exports = db;    
+module.exports = db;
